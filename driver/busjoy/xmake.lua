95327ec52bc3b352d5d9c3c2dbf95747e30994cd
alray <1780284652@qq.com>
2025-01-29 11:03:14 +0800

    Fix: 修复BusJoy.inx, Busjoy安装成功

--- driver/busjoy/xmake.lua
+++ driver/busjoy/xmake.lua
@@ -6,6 +6,6 @@ if is_os("windows") then
         add_values("wdk.tracewpp.flags", "-func:TraceEvents(LEVEL,FLAGS,MSG,...)", "-func:Hexdump((LEVEL,FLAGS,MSG,...))")
         add_includedirs("./include")
         add_files("**mof")
-        -- add_files("**inx|**inf")
+        add_files("**inx|**inf")
         add_files("./src/*.c", {rule = "wdk.tracewpp"})
 end
